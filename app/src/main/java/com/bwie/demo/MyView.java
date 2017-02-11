e91199f0e38dd80770cd816273039576b090bf10
chenwenmeng <1289196829@qq.com>
2017-02-11 10:05:39 +0800

    自定义各种基本类型

--- app/src/main/java/com/bwie/demo/MyView.java
+++ app/src/main/java/com/bwie/demo/MyView.java
@@ -8,10 +8,6 @@ import android.graphics.Canvas;
 
 import android.graphics.Color;
 import android.graphics.Paint;
-
-import android.graphics.Rect;
-
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
